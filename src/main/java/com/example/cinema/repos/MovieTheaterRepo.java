6aa0037d9e2746385131b34c7568bce5fb09f892
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2022-11-19 20:13:18 +0100

    c

--- src/main/java/com/example/cinema/repos/MovieTheaterRepo.java
+++ src/main/java/com/example/cinema/repos/MovieTheaterRepo.java
@@ -1,10 +1,19 @@
 package com.example.cinema.repos;
 
 import com.example.cinema.entities.MovieTheater;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.DefaultParameters;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 
 @RepositoryRestResource
 public interface MovieTheaterRepo extends JpaRepository<MovieTheater, Integer> {
+    @Query("select m from MovieTheater m where m.name  like :kw")
+    List<MovieTheater> searchMovieTheater(@Param("kw")  String keyword);
+
 }
--- src/main/java/com/example/cinema/ws/MovieTheaterRESTController.java
+++ src/main/java/com/example/cinema/ws/MovieTheaterRESTController.java
@@ -22,27 +22,32 @@ public class MovieTheaterRESTController {
     public List<MovieTheater> getMovieTheater(){
         return movieTheaterRepo.findAll();
     }
-    @PostMapping("/add")
+    @PostMapping("/create")
     public MovieTheater addMovieTheatre(@RequestBody MovieTheater movieTheater){
         return movieTheaterRepo.save(movieTheater);
     }
-    @PutMapping("/edit/{id}")
-    public MovieTheater editMovieTheatre(@PathVariable Integer id,MovieTheater movieTheater){
-        MovieTheater movieT = movieTheaterRepo.findById(id).get();
-        if(movieT != null){
-            movieT.setName(movieTheater.getName());
-            movieT.setAddress(movieTheater.getAddress());
-            movieT.setCreationDate(movieTheater.getCreationDate());
-            movieT.setChairsNumber(movieTheater.getChairsNumber());
-            movieT.setStatus(movieTheater.getStatus());
-            movieT.setType(movieTheater.getType());
-            return movieTheaterRepo.save(movieT);
+    @PutMapping("/update/{id}")
+    public MovieTheater editMovieTheatre(@PathVariable Integer id,@RequestBody MovieTheater mt){
+        MovieTheater movieTheater = movieTheaterRepo.findById(id).get();
+        if (movieTheater != null){
+            movieTheater.setName(mt.getName());
+            movieTheater.setAddress(mt.getAddress());
+            movieTheater.setCreationDate(mt.getCreationDate());
+            movieTheater.setChairsNumber(mt.getChairsNumber());
+            movieTheater.setStatus(mt.getStatus());
+            movieTheater.setType(mt.getType());
+            return movieTheaterRepo.save(movieTheater);
         }
-        return  null;
+        return null;
     }
     @DeleteMapping("/delete/{id}")
     public void deleteMovieTheatre(@PathVariable Integer id){
         movieTheaterRepo.deleteById(id);
+    }
+
+    @GetMapping("/movietheater/search")
+    public List<MovieTheater> searchMovieTheater(@RequestParam(name = "keyword",defaultValue = "")String keyword){
+        return movieTheaterRepo.searchMovieTheater("%"+keyword+"%");
     }
 
 }
